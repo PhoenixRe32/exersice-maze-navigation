eff8be8cd9b8b3425e0300f6e789df6c19d7194c
Andreas Andreou <aandreou@thoughtworks.com>
2019-02-09 16:54:05 +0000

    [Andreas] Create the enum representation of the maze blocks
    Each character represents a building block for the maze.
    Will enums to represent them internally because I like enums better than
    constant characters and strings.

---
+++ src/main/java/uk/gov/dwp/mazeexplorer/maze/MazeBlock.java
@@ -0,0 +1,37 @@
+package uk.gov.dwp.mazeexplorer.maze;
+
+import static java.util.stream.Collectors.toMap;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import uk.gov.dwp.mazeexplorer.maze.exceptions.InvalidMazeBlockRepresentation;
+
+public enum MazeBlock {
+    WALL('X'),
+    SPACE(' '),
+    ENTRY('S'),
+    EXIT('F');
+
+    private final Character representation;
+
+    private static final Map<Character, MazeBlock> mappings = Arrays.stream(MazeBlock.values())
+            .collect(toMap(MazeBlock::getRepresentation, mb -> mb));
+
+
+    public Character getRepresentation() {
+        return representation;
+    }
+
+    MazeBlock(Character representation) {
+        this.representation = representation;
+    }
+
+    public static MazeBlock fromCharacter(Character c) {
+        MazeBlock mazeBlock = mappings.get(c);
+        if (mazeBlock == null) {
+            throw new InvalidMazeBlockRepresentation(c + " is not a valid maze block.");
+        }
+        return mazeBlock;
+    }
+}
---
+++ src/main/java/uk/gov/dwp/mazeexplorer/maze/exceptions/InvalidMazeBlockRepresentation.java
@@ -0,0 +1,7 @@
+package uk.gov.dwp.mazeexplorer.maze.exceptions;
+
+public class InvalidMazeBlockRepresentation extends RuntimeException {
+    public InvalidMazeBlockRepresentation(String message) {
+        super(message);
+    }
+}
