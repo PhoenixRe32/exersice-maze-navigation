ea8d90f1201c424a6c9ee3a636ebfb4ddc6d92e6
Andreas Andreou <aandreou@thoughtworks.com>
2019-02-10 22:32:01 +0000

    [Andreas] Made a quick terminal interface so I can interact with it

--- src/main/java/uk/gov/dwp/mazeexplorer/Application.java
+++ src/main/java/uk/gov/dwp/mazeexplorer/Application.java
@@ -3,8 +3,10 @@ package uk.gov.dwp.mazeexplorer;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import uk.gov.dwp.mazeexplorer.physics.exceptions.InvalidDirection;
+
 public class Application {
-    public static void main(String... arguments) throws IOException, URISyntaxException {
+    public static void main(String... arguments) throws IOException, URISyntaxException, InvalidDirection {
         MazeExplorer mazeExplorer = new MazeExplorer();
         mazeExplorer.start();
     }
--- src/main/java/uk/gov/dwp/mazeexplorer/MazeExplorer.java
+++ src/main/java/uk/gov/dwp/mazeexplorer/MazeExplorer.java
@@ -3,7 +3,6 @@ package uk.gov.dwp.mazeexplorer;
 import static java.nio.file.Files.isRegularFile;
 import static java.util.stream.Collectors.toMap;
 
-import java.io.Console;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -11,28 +10,35 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Scanner;
 
 import uk.gov.dwp.mazeexplorer.explorer.Explorer;
 import uk.gov.dwp.mazeexplorer.maze.Maze;
+import uk.gov.dwp.mazeexplorer.physics.Coordinates;
+import uk.gov.dwp.mazeexplorer.physics.Position;
+import uk.gov.dwp.mazeexplorer.physics.exceptions.InvalidDirection;
 
 public class MazeExplorer {
 
     private static final String NEW_LINE = System.getProperty("line.separator");
 
     private static final String MAZES_DIRECTORY = "mazes";
-    
-    private static final String EXIT = "exit";
+
+    private static final String EXIT = "EXIT";
+    private static final String STEP = "STEP";
+    private static final String LEFT = "LEFT";
+    private static final String RIGHT = "RIGHT";
+    private static final String PEEK = "PEEK";
+    private static final String CHECK = "CHECK";
+    private static final String TRAIL = "TRAIL";
 
     private final Map<String, Maze> mazes;
+
     Map<String, Maze> getMazes() {
         return mazes;
-    }
-
-    private Explorer explorer;
-    Explorer getExplorer() {
-        return explorer;
     }
 
     public MazeExplorer() throws URISyntaxException, IOException {
@@ -56,27 +62,28 @@ public class MazeExplorer {
         return new Explorer(maze);
     }
 
-    public void start() {
-        Console console = System.console();
-        console.format("Welcome to maze explorer.").format(NEW_LINE)
+    public void start() throws InvalidDirection {
+        Scanner scanner = new Scanner(System.in);
+        System.out.format("Welcome to maze explorer.").format(NEW_LINE)
                 .format("You have the following mazes:").format(NEW_LINE);
-        mazes.keySet().forEach(mazeName -> console.format(mazeName).format(NEW_LINE));
+        mazes.keySet().forEach(mazeName -> System.out.format("\t* %s", mazeName).format(NEW_LINE));
 
-        String mazeName = console.readLine("What maze do you want to explore? ");
+        System.out.format("What maze do you want to explore?").format(NEW_LINE);
+        String mazeName = scanner.nextLine();
 
         Explorer explorer = createExplorer(mazeName);
-        if (this.explorer == null) {
+        if (explorer == null) {
             return;
         }
 
         loop(explorer);
     }
 
-    private void loop(Explorer explorer) {
-        Console console = System.console();
+    private void loop(Explorer explorer) throws InvalidDirection {
+        Scanner scanner = new Scanner(System.in);
         String instruction = "";
         while (!instruction.equalsIgnoreCase(EXIT)) {
-            instruction = console
+            System.out.format(NEW_LINE)
                     .format("You are at %s", explorer.getCurrentPosition()).format(NEW_LINE)
                     .format("You are allowed to:").format(NEW_LINE)
                     .format("\tStep: step forward").format(NEW_LINE)
@@ -86,9 +93,32 @@ public class MazeExplorer {
                     .format("\tCheck: see where you can move").format(NEW_LINE)
                     .format("\tTrail: print your trail so far").format(NEW_LINE)
                     .format("\tExit: end it").format(NEW_LINE).format(NEW_LINE)
-                    .readLine("What would you like to do? ")
-                    .toUpperCase();
-            console.format(instruction);
+                    .format("What would you like to do?").format(NEW_LINE);
+            instruction = scanner.nextLine().toUpperCase();
+            switch (instruction) {
+                case STEP:
+                    explorer.moveForward();
+                    break;
+                case RIGHT:
+                    explorer.turnRight();
+                    break;
+                case LEFT:
+                    explorer.turnLeft();
+                    break;
+                case PEEK:
+                    System.out.format("%s up ahead.", explorer.peekAhead());
+                    break;
+                case TRAIL:
+                    List<Position> trail = explorer.getTrail();
+                    trail.forEach(position -> System.out.format("\t* %s", position).format(NEW_LINE));
+                    break;
+                case CHECK:
+                    List<Coordinates> freeSpace = explorer.findFreeSpacesInTheSurroundingAreaToMoveTo();
+                    System.out.format("You can move to the following tiles:").format(NEW_LINE);
+                    freeSpace.forEach(coordinates -> System.out.format("\t* %s", coordinates).format(NEW_LINE));
+                default:
+                    break;
+            }
         }
     }
 }
